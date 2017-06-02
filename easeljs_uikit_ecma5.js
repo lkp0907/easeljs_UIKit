f14f91d07d49e2da9bd651142c11431d44dc0c47
ocsoon <gipyeong.lee@madorca.com>
2017-06-02 16:41:34 +0900

    Modified UIView, UIImageView, UIButton Class

--- easeljs_uikit_ecma5.js
+++ easeljs_uikit_ecma5.js
@@ -13,6 +13,14 @@ function UIContainer() {
 UIContainer.prototype = Object.create(createjs.Container.prototype);
 UIContainer.prototype.constructor = UIContainer;
 
+UIContainer.prototype.setHide = function(_isHide){
+	if(typeof _isHide === "undefined"){
+		this.visible = true;
+	}else{
+		this.visible = !_isHide;
+	}
+}
+
 function UIView(_x,_y,_width,_height) {
   // Call the parent constructor
 	UIContainer.call(this);
@@ -60,8 +68,8 @@ UIView.prototype.setBackgroundImage = function(path,callback){
 			
 		}
 	}
-	
 }
+
 UIView.prototype.setAlpha = function(_alpha){
 	var obj = this;
 	obj.background.alpha = _alpha;
@@ -101,7 +109,7 @@ function UIImageView(_x,_y,_width,_height) {
 	var _height = typeof _height !== 'undefined' ?  _height : 100;
 	
 	this.frame = {x:_x,y:_y,width:_width,height:_height};
-	this.color = "white";
+	this.color = undefined;
 	
 	this.background = new createjs.Shape();
 	this.background.name = "background";
@@ -115,6 +123,29 @@ function UIImageView(_x,_y,_width,_height) {
 }
 UIImageView.prototype = Object.create(createjs.Container.prototype);
 UIImageView.prototype.constructor = UIImageView;
+
+UIImageView.prototype.setFrame = function(_x,_y,_width,_height){
+	var obj = this;
+	this.setBounds (_x,_y,_width,_height);
+	if(this.color !== undefined){
+		this.background.graphics.clear();	
+	}
+	this.frame.x = _x;
+	this.frame.y = _y;
+	this.frame.width = _width;
+	this.frame.height = _height;
+	if(typeof this.color !== "undefined" || this.color !== undefined){
+		this.background.graphics.beginFill(this.color).drawRect(0, 0, this.frame.width, this.frame.height);	
+	}
+	this.background.setBounds(0,0,_width,_height);
+	
+	this.x = _x;
+	this.y = _y;
+	
+	if(obj.stage !== undefined && obj.stage !== null){
+		obj.stage.update();	
+	}
+}
 
 UIImageView.prototype.setBackgroundColor = function(_color){
 	var obj = this;
@@ -160,6 +191,7 @@ UIImageView.prototype.setScale = function(_x,_y){
 UIImageView.prototype.setBackgroundImage = function(path,callback){
 	var obj = this;
 	obj.background.graphics.clear();
+	obj.color = undefined;
 	if(typeof path === "object"){
 		obj.background.graphics.beginBitmapFill(path, 'no-repeat').drawRect(0, 0, obj.frame.width, obj.frame.height);
 		if(obj.stage !== undefined && obj.stage !== null){
@@ -200,7 +232,7 @@ function UIButton(_x,_y,_width,_height) {
 	
 	this.background = new createjs.Shape();
 	this.background.name = "background";
-	this.background.graphics.beginFill(this.color).drawRect(0, 0, _width, _height);
+	//this.background.graphics.beginFill(this.color).drawRect(0, 0, _width, _height);
 	
 	this.fontProperty = {type:"bold",size:"12px",font:"Arial"};
 	
@@ -264,7 +296,12 @@ UIButton.prototype.setScale = function(_x,_y){
 }
 UIButton.prototype.setBackgroundColor = function(_color){
 	var obj = this;
-	obj.background.graphics.beginFill(_color).drawRect(0, 0, this.frame.width, this.frame.height);
+	if(_color == undefined || typeof _color === "undefined"){
+		obj.background.graphics.clear();
+	}
+	else{
+		obj.background.graphics.beginFill(_color).drawRect(0, 0, this.frame.width, this.frame.height);
+	}
 	if(obj.stage !== undefined && obj.stage !== null){
 		obj.stage.update();	
 	}
@@ -272,7 +309,10 @@ UIButton.prototype.setBackgroundColor = function(_color){
 UIButton.prototype.setBackgroundImage = function(path,callback){
 	var obj = this;
 	obj.background.graphics.clear();
-	if(typeof path === "object"){
+	if(path === undefined || typeof path === "undefined"){
+		
+	}
+	else if(typeof path === "object"){
 		obj.background.graphics.beginBitmapFill(path, 'no-repeat').drawRect(0, 0, obj.frame.width, obj.frame.height);
 		obj.autoAlignTextLabel();
 		if(obj.stage !== undefined && obj.stage !== null){
@@ -309,21 +349,22 @@ UIButton.prototype.addTarget = function(state,handler){
 	var obj = this;
 	var originY = obj.y;
 	var originX = obj.x;
-	if(this.hasEventListener(state)){
-		this.removeEventListener (state);
+	if(!this.hasEventListener(state)){
+		this.addEventListener(state, function(event) {
+			obj.y += 2;
+			obj.x -= 2;
+			handler(event);
+			setTimeout(function(){
+				obj.y = originY;
+				obj.x = originX;
+			},200);
+			event.preventDefault();
+		});		
+	}else{
+
 	}
-	this.addEventListener(state, function(event) {
-		obj.y += 2;
-		obj.x -= 2;
-		handler(event);
-		setTimeout(function(){
-			obj.y = originY;
-			obj.x = originX;
-		},200);
-	});
 }
 UIButton.prototype.setText = function(_text){
-	
 	this.textLabel.text = _text;
 	this.autoAlignTextLabel();
 }
@@ -381,16 +422,27 @@ function UILabel(_x,_y,_width,_height) {
 	this.background.name = "background";
 	this.background.graphics.beginFill("white").drawRect(0, 0, _width, _height);
 	
-	this.textLabel = new createjs.Text("click", "bold 12px Arial", "#000000");
+	this.outlineLabel = new createjs.Text("", "bold 12px Arial", "#000000");
+	
+	this.outlineLabel.name = "outlineLabel";
+	this.outlineLabel.textAlign = "center";
+	this.outlineLabel.textBaseline = "middle";
+	this.outlineLabel.color = "black";
+	this.outlineLabel.visible = false;
+	this.outlineLabel.text = "";
+	
+	
+	this.textLabel = new createjs.Text("", "bold 12px Arial", "#000000");
 	this.textLabel.name = "textLabel";
 	this.textLabel.textAlign = "center";
 	this.textLabel.textBaseline = "middle";
 	this.textLabel.alpha = 1;
+	this.textLabel.text = "";
 	
-	this.outlineLabel = undefined;
+	this.outlineWidth = 0;
 	
 	this.autoAlignTextLabel();
-	this.addChild(this.background,this.textLabel,this.outlineLabel);
+	this.addChild(this.background,this.outlineLabel,this.textLabel);
 	this.x = _x;
 	this.y = _y;
 		
@@ -401,48 +453,72 @@ function UILabel(_x,_y,_width,_height) {
 UILabel.prototype = Object.create(createjs.Container.prototype);
 UILabel.prototype.constructor = UILabel;
 
-UILabel.prototype.setTextAlign = function(_align){
+UILabel.prototype.setFrame = function(_x,_y,_width,_height){
+	var obj = this;
+	this.setBounds (_x,_y,_width,_height);
+	this.frame.x = _x;
+	this.frame.y = _y;
+	this.frame.width = _width;
+	this.frame.height = _height;
 	
+	this.x = _x;
+	this.y = _y;
+	
+	if(obj.stage !== undefined && obj.stage !== null){
+		obj.stage.update();	
+	}
+}
+
+UILabel.prototype.setTextAlign = function(_align){
+	var obj = this;
 	this.textLabel.textAlign = _align;
 	if(typeof obj.outlineLabel !== "undefined"){
 		this.outlineLabel.textAlign = _align;
 	}
+	if(obj.stage !== undefined && obj.stage !== null){
+		obj.stage.update();	
+	}
 }
 UILabel.prototype.setText = function(_text){
+	var obj = this;
 	this.textLabel.text = _text;
-	if(typeof obj.outlineLabel !== "undefined"){
+	if(typeof obj.outlineLabel !== "undefined" || obj.outlineLabel !== undefined){
 		this.outlineLabel.text = _text;
+	}
+	obj.autoAlignTextLabel();
+	if(obj.stage !== undefined && obj.stage !== null){
+		obj.stage.update();	
 	}
 }
 UILabel.prototype.setOutlineColor = function(_color){
-	if(typeof this.outlineLabel === "undefined"){
-		this.outlineLabel = new createjs.Text("", "bold 12px Arial", "#000000");
-		this.outlineLabel.name = "textLabel";
-		this.outlineLabel.textAlign = "center";
-		this.outlineLabel.textBaseline = "middle";
-		this.outlineLabel.alpha = 0;
+	var obj = this;
+	if(typeof this.outlineLabel === false){
+		this.outlineLabel.visible = true;
 	}
-	if(typeof _color === "undefined"){
-		this.outlineLabel.alpha = 0;
+	if(typeof _color === "undefined" || _color === undefined){
+		this.outlineLabel.visible = false;
 		return;
 	}
 	this.outlineLabel.color = _color;
+	
+	if(obj.stage !== undefined && obj.stage !== null){
+		obj.stage.update();	
+	}
 }
 UILabel.prototype.setOutline = function(_width){
 	var obj = this;
-	if(typeof this.outlineLabel === "undefined"){
-		this.outlineLabel = new createjs.Text("", "bold 12px Arial", "#000000");
-		this.outlineLabel.name = "textLabel";
-		this.outlineLabel.textAlign = "center";
-		this.outlineLabel.textBaseline = "middle";
-		this.outlineLabel.alpha = 0;
+	if(this.outlineLabel.visible === false){
+		this.outlineLabel.visible = true;
 	}
 	if(typeof _width == "undefined" || _width == 0){
-		this.outlineLabel.alpha = 0;
+		this.outlineLabel.visible = false;
 	}
-	this.outlineLabel.alpha = 1;
+	this.outlineWidth = _width;
+	this.outlineLabel.text = obj.textLabel.text;
 	this.outlineLabel.outline = _width;
 	
+	
+	this.autoAlignTextLabel();
 	if(obj.stage !== undefined && obj.stage !== null){
 		obj.stage.update();	
 	}
@@ -495,11 +571,13 @@ UILabel.prototype.autoAlignTextLabel = function(){
 	obj.textLabel.y = (obj.frame.height - b.height)/2-b.y;
 		
 		
-	if(typeof obj.outlineLabel !== "undefined"){
+	if(typeof obj.outlineLabel !== "undefined" || obj.outlineLabel !== undefined){
 		obj.outlineLabel.x = (obj.frame.width - b.width)/2-b.x;
 		obj.outlineLabel.y = (obj.frame.height - b.height)/2-b.y;	
 	}
-	
+	if(obj.stage !== undefined && obj.stage !== null){
+		obj.stage.update();	
+	}
 }
 
 UILabel.prototype.fontize = function(){
@@ -511,9 +589,8 @@ UILabel.prototype.fontize = function(){
 	var font = this.fontProperty.type +" "+ size + " " + this.fontProperty.font;
 	
 	this.textLabel.font = font;
-	if(typeof obj.outlineLabel !== "undefined"){
-		this.outlineLabel.font = font;
-	}
+	this.outlineLabel.font = font;
+	
 	
 	if(obj.stage !== undefined && obj.stage !== null){
 		obj.stage.update();	
